d3577489317e0d33ca2d1c8547bcbfa44238f78b
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-07-25 15:43:05 +0300

    added toggle in description

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+      $(".design").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#development").click(function(){
+      $(".development").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#product").click(function(){
+      $(".product").toggle();
+    });
+  });
+
+
+
+  
