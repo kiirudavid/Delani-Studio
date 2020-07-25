517e22803dd680124de2337118681215607017db
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-07-25 20:42:42 +0300

    added an alert to emails received

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,24 @@ $(document).ready(function(){
 
 
 
+
+  $(function () {
+    $("form#form").on('submit',function(event){
+       event.preventDefault();
+       let name = $("input#name").val();
+      
+       if ($("input#name").val()){
+           alert ("Hey " + name + ", Thanks for reaching out to us,Will get back to you");
+       }
+       else {
+           alert("Please provide your correct name and email!");
+       }
+
+   });
+
+
+
+});
+
+
   
