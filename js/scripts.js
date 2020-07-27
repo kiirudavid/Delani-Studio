a6bf463eeaa3d948da88297f724895aa70bfbbfd
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-07-27 07:58:13 +0300

    updated portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,44 @@
-$(document).ready(function(){
-    $("#design").click(function(){
-      $(".design").toggle();
-    });
-  });
+// $(document).ready(function(){
+//     $("#design").click(function(){
+//       $(".design").toggle();
+//     });
+//   });
 
-  $(document).ready(function(){
-    $("#development").click(function(){
-      $(".development").toggle();
-    });
-  });
+//   $(document).ready(function(){
+//     $("#development").click(function(){
+//       $(".development").toggle();
+//     });
+//   });
 
-  $(document).ready(function(){
-    $("#product").click(function(){
-      $(".product").toggle();
-    });
-  });
+//   $(document).ready(function(){
+//     $("#product").click(function(){
+//       $(".product").toggle();
+//     });
+//   });
+
+$('#image').on("click",function(e){
+  $('.span').toggle('slow');
+  $('.imagE').toggle("click");
+ });
+$('#image1').on("click",function(e){
+  $('.span1').toggle('slow');
+  $('.images1').toggle("click");
+});
+$('#image2').on("click",function(e){
+  $('.span2').toggle('slow');
+  $('.images2').toggle("click");
+});$('#image').on("click",function(e){
+  $('.span').toggle('slow');
+  $('.imagE').toggle("click");
+ });
+$('#image1').on("click",function(e){
+  $('.span1').toggle('slow');
+  $('.images1').toggle("click");
+});
+$('#image2').on("click",function(e){
+  $('.span2').toggle('slow');
+  $('.images2').toggle("click");
+});
 
 
 
