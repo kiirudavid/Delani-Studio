d714bc20651c88c2f97b81885ea4cc04585c2dc7
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-07-28 21:41:03 +0300

    indented project

--- js/scripts.js
+++ js/scripts.js
@@ -17,67 +17,38 @@
 //   });
 
 
-$(".click1").click(function(){
+$(".click1").click(function () {
   $(".hide1").slideToggle(1000);
   $(".show1").slideToggle(1200);
 });
 
-$(".click2").click(function(){
+$(".click2").click(function () {
   $(".hide2").slideToggle(1000);
   $(".show2").slideToggle(1200);
 });
 
-$(".click3").click(function(){
+$(".click3").click(function () {
   $(".hide3").slideToggle(1000);
   $(".show3").slideToggle(1200);
 });
 
 
-// $('#image').on("click",function(e){
-//   $('.span').toggle('slow');
-//   $('.imagE').toggle("click");
-//  });
-// $('#image1').on("click",function(e){
-//   $('.span1').toggle('slow');
-//   $('.images1').toggle("click");
-// });
-// $('#image2').on("click",function(e){
-//   $('.span2').toggle('slow');
-//   $('.images2').toggle("click");
-  
-// });$('#image').on("click",function(e){
-//   $('.span').toggle('slow');
-//   $('.imagE').toggle("click");
-//  });
-// $('#image1').on("click",function(e){
-//   $('.span1').toggle('slow');
-//   $('.images1').toggle("click");
-// });
-// $('#image2').on("click",function(e){
-//   $('.span2').toggle('slow');
-//   $('.images2').toggle("click");
-// });
+$(function () {
+  $("form#form").on('submit', function (event) {
+    event.preventDefault();
+    let name = $("input#name").val();
 
+    if ($("input#name").val()) {
+      alert("Hey " + name + ", Thanks for reaching out to us,Will get back to you");
+    }
+    else {
+      alert("Please provide your correct name and email!");
+    }
 
-
-
-  $(function () {
-    $("form#form").on('submit',function(event){
-       event.preventDefault();
-       let name = $("input#name").val();
-      
-       if ($("input#name").val()){
-           alert ("Hey " + name + ", Thanks for reaching out to us,Will get back to you");
-       }
-       else {
-           alert("Please provide your correct name and email!");
-       }
-
-   });
+  });
 
 
 
 });
 
 
-  
